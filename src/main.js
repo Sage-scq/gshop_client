26e56fc673ae3727295b17f181dd92db204dee05
喷火的火龙果 <1637718646@qq.com>
2021-09-16 15:02:31 +0800

    完成home组件

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import TypeNav from "@/components/TypeNav";
+
 // 不显示非生产环境提示
 Vue.config.productionTip = false
-
+// 注册全局组件TypeNav
+Vue.component(TypeNav.name, TypeNav)
 new Vue({
   render: h => h(App),
   router
--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,15 +1,28 @@
 <template>
-  <div class="home">Home</div>
+  <div class="home">
+    <TypeNav />
+    <ListContainer />
+    <TodayRecommend />
+    <Like />
+    <Rank />
+    <Floor />
+    <Floor />
+    <Brand />
+  </div>
 </template>
 
 <script>
+import ListContainer from "./ListContainer";
+import TodayRecommend from "./TodayRecommend";
+import Brand from "./Brand/Brand.vue";
+import Floor from "./Floor/Floor.vue";
+import Rank from "./Rank/Rank.vue";
+import Like from "./Like/Like.vue";
 export default {
   name: "Home",
+  components: { ListContainer, TodayRecommend, Brand, Floor, Rank, Like },
 };
 </script>
 
 <style lang="less" scoped>
-.home {
-  color: red;
-}
 </style>
